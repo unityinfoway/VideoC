26e7254a322c2646acc4907dbd053ef73d42559c
Ayush Chaturvedi <ayushji456789@gmail.com>
2025-07-22 14:39:47 +0530

    Saving my latest work

--- App/vite.config.js
+++ App/vite.config.js
@@ -1,20 +1,45 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: './',
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       }
+//     },
+//     watch: {
+//       usePolling: true,
+//       interval: 100,
+//     }
+//   },
+//   plugins: [react()],
+// });
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'development' ? '/' : './',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+        }
+      },
+      watch: {
+        usePolling: true,
+        interval: 100,
       }
-    },
-    watch: {
-      usePolling: true,
-      interval: 100,
     }
-  },
-  plugins: [react()],
+  };
 });
